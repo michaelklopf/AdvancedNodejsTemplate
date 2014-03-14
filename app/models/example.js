bf26764fc797b2f400fbd4a4cf861b4e5924b1b8
Michael Klopf <klopf.michael@gmail.com>
2014-03-14 06:54:26 +0100

    Fixed a bug in API, changed routes structure.

--- app/models/example.js
+++ app/models/example.js
@@ -1,4 +1,5 @@
 // app/models/example.js
+
 var mongoose    = require('mongoose');
 
 var exampleSchema = new mongoose.Schema({
--- app/routes.js
+++ app/routes.js
@@ -1,84 +1,8 @@
 // app/routes.js
 
-// load jade for html processing
-
-// load models
-var Example     = require('./models/example');
-
 // expose routes
 module.exports = function(app) {
-    /*
-    app.get( '/api', function( request, response ) {
-	   response.send( 'Library API is running' );
-    });
-    */
-    
-    // methods to manipulate data
-    app.get('/examples', function(req, res) {
-        return Example.find(function(err, examples) {
-            if(!err) {
-                return res.send(examples);
-            } else {
-                return console.log(err);
-            }
-        });
-    });
-    
-    app.post('/examples', function(req, res) {
-        var example = new Example({
-            name: req.body.name,
-            mail: req.body.mail
-        });
-        example.save(function(err) {
-            if (!err) {
-                return console.log('created');
-            } else {
-                return consoel.log(err);
-            }
-            return res.send(example);
-        });
-    });
-    
-    app.get('/examples/:id', function(req, res) {
-        return Example.findById(req.params.id, function(err, example) {
-            if(!err) {
-                return res.send(example);
-            } else {
-                return console.log(err);
-            }
-        });
-    });
-        
-    app.put('/examples/:id', function(req, res) {
-        console.log('Updating example ' + req.body.name);
-        return Example.findById(req.params.id, function(err, example) {
-            example.name = req.body.name;
-            example.mail = req.body.mail;
-            
-            return example.save(function(err) {
-                if(!err) {
-                    console.log('example updated');
-                } else {
-                    console.log(err);
-                }
-                return res.send(example);
-            });
-        });
-    });
-        
-    app.delete('/examples/:id', function(req, res) {
-        console.log('Deleting example with id: ' + req.params.id);
-        return Example.findById(req.params.id, function(err, example) {
-            return example.remove(function(err) {
-                if(!err) {
-                    console.log('Example removed');
-                    return res.send('');
-                } else {
-                    console.log(err);
-                }
-            });
-        });
-    });
+    require('./routes/examples.js')(app);
     
     // application routes
     app.get('/', function(req, res) {
